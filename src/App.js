3d10bceed1b7cbb789abc19962cda52c2522edc3
vladimir <lovkiy2012@gmail.com>
2019-12-18 23:24:24 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Layout from "./hoc/layout/layout";
+import Quiz from "./containers/Quiz/Quiz";
 
 function App() {
-  return <Layout>
-
-
-    <div style={{width:400,border:'1px solid black'}}>
-      <h1>layout worck</h1>
-    </div>
-  </Layout>;
+  return (
+    <Layout>
+      <Quiz />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./Quiz.module.css";
+class Quiz extends Component {
+  render() {
+    return <div className={classes.Quiz}>
+        <h1>Quiz</h1>
+    </div>;
+  }
+}
+export default Quiz;
