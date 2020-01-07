3ba8dbda45c2a6ec0e7b1a34d24013ed8d603aa6
vladimir <lovkiy2012@gmail.com>
2020-01-07 21:45:49 +0300

    103 customization

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom'
-const app=(
-    <BrowserRouter>
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducers from "./store/reducers/rootReducers";
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk'
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const store = createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
+const app = (
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
-)
-ReactDOM.render(app, document.getElementById('root'));
+  </BrowserRouter>
+);
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
